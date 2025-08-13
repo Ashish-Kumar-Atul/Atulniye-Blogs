42a285ded697cd4dcd656611d0d1e984519d079f
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-08-13 11:30:16 +0530

    Custom hooks added

---
+++ client/src/hooks/useAuth.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useAuth(){
+    const [loading, setLoading] = useState(true);
+    const [authenticated, setAuthenticated] = useState(false);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/auth/status',{withCredentials: true})
+        .then(res => {
+            setAuthenticated(true);
+            setUser(res.data.user);
+        })
+        .catch(() => {
+            setAuthenticated(false);
+            setUser(null);
+        })
+        .finally(()=>{
+            setLoading(false);
+        })
+    },[]);
+    return { authenticated, user, loading }
+}

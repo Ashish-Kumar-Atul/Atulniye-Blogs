03908303eba7e5c4076a51b00cbe832f49c4e91d
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-09-24 02:36:24 +0530

    server online host added

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/authRoutes.js');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'], 
+  origin: ['http://localhost:5173', 'http://localhost:5174','https://atulniye-blogs.onrender.com/'], 
   credentials: true,              
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));

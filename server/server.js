65f6c1f5da08eaad13c67e8c38b134f43574a244
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-09-24 04:37:28 +0530

    server cors updated

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const authRoutes = require('./routes/authRoutes.js');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','https://clever-crumble-50aae3.netlify.app'], 
+  origin: ['https://clever-crumble-50aae3.netlify.app','http://localhost:5173', 'http://localhost:5174'], 
   credentials: true,              
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));

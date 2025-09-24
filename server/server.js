d55e08105d9ed225a9640758e5c4f6b5b2f36bb4
Ashish Kumar Atul <162558245+Ashish-Kumar-Atul@users.noreply.github.com>
2025-09-24 06:08:26 +0530

    backend link updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.set('trust proxy', 1);
 
 app.use(cors({
   origin: [
-    'https://lively-tapioca-1b9cae.netlify.app',
+    'https://atulniye-blogs.netlify.app/',
     'http://localhost:5173',
     'http://localhost:5174'
   ],
